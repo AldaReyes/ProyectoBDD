c0d92dd88044131bc3e7d5bb5e4660ece6ac4dd0
ArielGonzalezVK125 <149214648+VergilKusanagiAGO125@users.noreply.github.com>
2024-05-06 22:53:05 -0600

    Add files via upload

--- interfaz.py
+++ interfaz.py
@@ -13,7 +13,7 @@ class appWindow(QWidget):#Clase Que Hereda QWidget
     def contenido(self):
         self.colorActual = None
         self.panelesGuardados_1 = []#Lista Vacia De Paneles
-        self.panelesGuardados_2 = []#Lista Vacia De Paneles
+        self.panelesGuardados_1_1 = []#Lista Vacia De Paneles
         
         self.nombreBotones_1 = ['Inventarios', 'Capital Humano', 'Localizaciones','Transacciones','Salir']#Lista De Nombres De Botones
         self.coloresBotones_1 = [QColor(200 + -(20 * (x + -1)), 200 + -(20 * (x + -1)), 200 + -(20 * (x + -1))) for x in range(len(self.nombreBotones_1))]#Lista De Colores De Botones
@@ -26,52 +26,53 @@ class appWindow(QWidget):#Clase Que Hereda QWidget
             self.boton_1.setStyleSheet(f'background-color: {self.colorActual.name()}; border: 1px solid black;')#Color Y Margen
             self.boton_1.clicked.connect(lambda _, id = indexBoton_1: self.selectPaneles1(id))#Eventos De Boton Con ID
         
-        for nombreBoton_1, colorPanel_1 in zip(self.nombreBotones_1,self.coloresBotones_1):#
-            self.frame_1 = QFrame(self)
-            self.frame_1.setFrameShape(QFrame.StyledPanel)
-            self.frame_1.move(140,20)
-            self.frame_1.setFixedSize(690,440)
-            self.frame_1.setStyleSheet(f'background-color: {colorPanel_1.name()}; border: 1px solid black;')  # Acceder al nombre del color
-            self.frame_1.hide()
-            self.panelesGuardados_1.append(self.frame_1)
+        for nombreBoton_1, colorPanel_1 in zip(self.nombreBotones_1,self.coloresBotones_1):#Ciclo Para Emparejar Listas
+            self.paneles_1 = QFrame(self)#Añadir En Panel Principal
+            self.paneles_1.setFrameShape(QFrame.StyledPanel)#Definir Bordes
+            self.paneles_1.move(140,20)#Posicion De Panel
+            self.paneles_1.setFixedSize(690,440)#Definir Tamaño
+            self.paneles_1.setStyleSheet(f'background-color: {colorPanel_1.name()}; border: 1px solid black;')#Definir Color Y Estilo
+            self.paneles_1.hide()#Esconder Paneles
+            self.panelesGuardados_1.append(self.paneles_1)#Añadir A Lista 1_1
             
-        self.nombreBotones_2 = ['Crear','Leer','Actualizar','Eliminar']#Lista De Nombres De Botones
-        self.coloresBotones_2 = [QColor(200 + -(20 * (x + -1)), 200 + -(20 * (x + -1)), 200 + -(20 * (x + -1))) for x in range(len(self.nombreBotones_2))]#Lista De Colores De Botones
+        self.nombreBotones_1_1 = ['Crear','Leer','Actualizar','Eliminar']#Lista De Nombres De Botones
+        self.coloresBotones_1_1 = [QColor(200 + -(20 * (x + -1)), 200 + -(20 * (x + -1)), 200 + -(20 * (x + -1))) for x in range(len(self.nombreBotones_1_1))]#Lista De Colores De Botones
         
-        for nombreBoton_2, colorPanel_2 in zip(self.nombreBotones_2,self.coloresBotones_2):#
-            self.frame_2 = QFrame(self.panelesGuardados_1[1])
-            self.frame_2.setFrameShape(QFrame.StyledPanel)
-            self.frame_2.move(20,100)
-            self.frame_2.setFixedSize(550,320)
-            self.frame_2.setStyleSheet(f'background-color: {colorPanel_2.name()}; border: 1px solid black;')
-            self.frame_2.hide()
-            self.panelesGuardados_2.append(self.frame_2)
+        for nombreBoton_1_1, colorPanel_1_1 in zip(self.nombreBotones_1_1,self.coloresBotones_1_1):#Ciclo Para Emparejar Listas
+            self.paneles_1_1 = QFrame(self.panelesGuardados_1[1])#Añadir En Panel 1 
+            self.paneles_1_1.setFrameShape(QFrame.StyledPanel)#Definir Bordes
+            self.paneles_1_1.move(20,80)#Posicion De Panel
+            self.paneles_1_1.setFixedSize(650,340)#Definir Tamaño
+            self.paneles_1_1.setStyleSheet(f'background-color: {colorPanel_1_1.name()}; border: 1px solid black;')#Definir Color Y Estilo
+            self.paneles_1_1.hide()#Esconder Paneles
+            self.panelesGuardados_1_1.append(self.paneles_1_1)#Añadir A Lista 1_1
             
-        self.comboBoxPanel_1 = QComboBox(self.panelesGuardados_1[1])
-        self.comboBoxPanel_1.addItems(self.nombreBotones_2)
-        self.comboBoxPanel_1.move(20, 40)
-        self.comboBoxPanel_1.setFixedSize(80, 20)
-        self.comboBoxPanel_1.currentIndexChanged.connect(self.selectPaneles2)
+        self.comboBoxPanel_1 = QComboBox(self.panelesGuardados_1[1])#Añadir En Panel 1_1
+        self.comboBoxPanel_1.addItems(self.nombreBotones_1_1)#Añadir Items De Lista
+        self.comboBoxPanel_1.move(20, 40)#Posicion De ComboBox
+        self.comboBoxPanel_1.setFixedSize(80, 20)#Definir Tamaño
+        self.comboBoxPanel_1.currentIndexChanged.connect(self.selectPaneles2)#Cuando Cambie De Opcion Cambia De Pestaña
         
         self.selectPaneles2(0)
         
-        for i in range(len(self.panelesGuardados_1)):
+        for i in range(len(self.panelesGuardados_1)):#Ciclo Para Titulo De Areas En Cada Panel_1
             self.titulo_1 = QLabel(f'Area: {self.nombreBotones_1[i]}',self.panelesGuardados_1[i])#Crear Label
             self.titulo_1.move(20, 20)#Posicion Del widget
             self.titulo_1.setStyleSheet(f'border: 0px;')#Sin Bordes
-        
-        self.texto_2 = QLabel('Amdios Tonotos',self.panelesGuardados_2[1])
-        self.texto_2.move(20, 20)
-        self.texto_2.setStyleSheet(f'border: 0px;')
+            
+        for i in range(len(self.panelesGuardados_1_1)):#Ciclo Para Titulo De ComboBox En Cada Panel_1_1
+            self.titulo_1 = QLabel(f'Area: {self.nombreBotones_1_1[i]}',self.panelesGuardados_1_1[i])#Crear Label
+            self.titulo_1.move(20, 20)#Posicion Del widget
+            self.titulo_1.setStyleSheet(f'border: 0px;')#Sin Bordes
         
     def cajaMensajes(self, x):
         if x == 1:
-            cajaMensaje_1 = QMessageBox()
-            cajaMensaje_1.setWindowTitle('Bienvenido')
-            cajaMensaje_1.setText('Ponga Aceptar Para Continuar')
-            botonMensaje_1 = QPushButton('Aceptar', cajaMensaje_1)
-            cajaMensaje_1.addButton(botonMensaje_1, QMessageBox.AcceptRole)
-            cajaMensaje_1.exec_()
+            cajaMensaje_1 = QMessageBox()#Mensaje De Cajita
+            cajaMensaje_1.setWindowTitle('Bienvenido')#Titulo De Cajita
+            cajaMensaje_1.setText('Ponga Aceptar Para Continuar')#Mensaje De Cajita
+            botonMensaje_1 = QPushButton('Aceptar', cajaMensaje_1)#Boton De Cajita
+            cajaMensaje_1.addButton(botonMensaje_1, QMessageBox.AcceptRole)#Añadir Boton A Cajita
+            cajaMensaje_1.exec_()#Iniciar Cajita
     
     def selectPaneles1(self, id):
         for panel in self.panelesGuardados_1:#Ciclo En Todos Los Paneles
@@ -84,10 +85,10 @@ class appWindow(QWidget):#Clase Que Hereda QWidget
             self.close()
 
     def selectPaneles2(self, id):
-        for panel in self.panelesGuardados_2:#Ciclo En Todos Los Paneles
+        for panel in self.panelesGuardados_1_1:#Ciclo En Todos Los Paneles
             panel.hide()#Oculta Los Paneles
-        if id < (len(self.panelesGuardados_2)):#Si El Tamaño De La Lista Es Menor A La ID
-            panelActual = self.panelesGuardados_2[id]#Selecciona El Panel Con El ID
+        if id < (len(self.panelesGuardados_1_1)):#Si El Tamaño De La Lista Es Menor A La ID
+            panelActual = self.panelesGuardados_1_1[id]#Selecciona El Panel Con El ID
             panelActual.show()#Mostrar Panel Selecionado
         else:
             pass
